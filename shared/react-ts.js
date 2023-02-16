d1af82e6e51db544122ae09d92cbbc7ec85defa5
Joseph Akhenda <akhenda@gmail.com>
2023-02-16 20:25:39 +0300

    feat(react): 🎸 remove tailwind plugin

--- shared/react-ts.js
+++ shared/react-ts.js
@@ -1,10 +1,10 @@
 module.exports = {
-  extends: ['airbnb', 'airbnb-typescript', 'plugin:tailwindcss/recommended', 'plugin:react/jsx-runtime'], // consider replacing Airbnb with Shopify's config
+  extends: ['airbnb', 'airbnb-typescript', 'plugin:react/jsx-runtime'], // consider replacing Airbnb with Shopify's config
   parserOptions: {
     ecmaFeatures: { jsx: true },
     project: './tsconfig.base.json',
   },
-  plugins: ['react', 'react-hooks', 'tailwindcss'],
+  plugins: ['react', 'react-hooks'],
   rules: {},
   settings: {
     react: { version: 'detect' },
